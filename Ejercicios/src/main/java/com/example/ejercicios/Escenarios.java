945781080b0378afad5ed6ea8d62e8e774707cd6
Apedlop <angelapedreralopez@gmail.com>
2024-09-26 18:59:33 +0200

    26/09/2024, ejer ContadorPulsacionesSimultaneo completado

--- Ejercicios/src/main/java/com/example/ejercicios/Escenarios.java
+++ Ejercicios/src/main/java/com/example/ejercicios/Escenarios.java
@@ -3,6 +3,7 @@ package com.example.ejercicios;
 import javafx.application.Application;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -14,31 +15,25 @@ import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-import javax.swing.event.ChangeListener;
-
-public class ContadorPulsaciones extends Application {
+public class Escenarios extends Application {
 
     @Override
     public void start(Stage escenarioPrincipal) {
         try {
-            ContadorPulsaciones2 escena1 = new ContadorPulsaciones2();
-            ContadorPulsaciones2 escena2 = new ContadorPulsaciones2();
+            // Crea dos instancias de la clase ContadorPulsaciones
+            ContadorPulsaciones escena1 = new ContadorPulsaciones();
+            ContadorPulsaciones escena2 = new ContadorPulsaciones();
 
-            IntegerProperty numPuls1 = new SimpleIntegerProperty(escena1.getNumContador());
-            IntegerProperty numPuls2 = new SimpleIntegerProperty(escena2.getNumContador());
+            // Obtener el número del contador
+            IntegerProperty numPuls1 = escena1.contPuls();
+            IntegerProperty numPuls2 = escena2.contPuls();
 
+            // Enlazamos bidireccionalmente los contadores
             numPuls1.bindBidirectional(numPuls2);
-            numPuls2.bindBidirectional(numPuls1);
 
-            escena1.setStage();
-            escena2.setStage();
-
-            escena1.contPuls().addListener(new ChangeListener(){
-                @Override
-                public void changed(ObservableValue o, Object oldVal, Object newVal){
-                    System.out.println("Electric bill has changed!");
-                }
-            });
+            // Mostramos los escenarios
+            escena1.setStage("Contador 1");
+            escena2.setStage("Contador 2");
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -49,27 +44,23 @@ public class ContadorPulsaciones extends Application {
     }
 }
 
-class ContadorPulsaciones2 {
+class ContadorPulsaciones {
 
-    public int numContador = 0;  // Inicializa el contador
+    private IntegerProperty numContador = new SimpleIntegerProperty(0);  // Inicializa el contador
     public Label lbContador;  // Define el Label de manera global para acceder desde los métodos
-    IntegerProperty numPuls = new SimpleIntegerProperty();
 
-    public int getNumContador() {
-        return numContador;
-    }
-
+    // Método para obtener la propiedad del contador
     public IntegerProperty contPuls() {
-        return numPuls;
+        return numContador;
     }
 
     // CONTADOR FUNCIONAL
     private void contador(int num) {
-        numContador = (num == 0) ? 0 : numContador + num;
-        lbContador.setText(String.valueOf(numContador));
+        numContador.set((num == 0) ? 0 : numContador.get() + num);
+        lbContador.setText(String.valueOf(numContador.get()));
     }
 
-    public void setStage() {
+    public void setStage(String nomVentana) {
         try {
             Stage escenarioPrincipal = new Stage();
 
@@ -110,9 +101,17 @@ class ContadorPulsaciones2 {
             btCero.setId("btCero");
 
             // CREAR CONTADOR
-            lbContador = new Label("0");  // Empieza en 0
+            lbContador = new Label(String.valueOf(numContador.get()));  // Empieza en 0
             lbContador.setFont(Font.font(30));
             lbContador.setId("lbContador");
+
+            // Añadir cambios en la propiedad numContador para actualizar el label
+            numContador.addListener(new ChangeListener<Number>() {
+                @Override
+                public void changed(ObservableValue<? extends Number> observable, Number oldVal, Number newVal) {
+                    lbContador.setText(String.valueOf(newVal));
+                }
+            });
 
             // AÑADIR BOTONES Y CONTADOR A LA INTERFAZ
             panelBotones.getChildren().addAll(btMas, btMenos, btCero);
@@ -121,7 +120,7 @@ class ContadorPulsaciones2 {
             // CREAR ESCENA
             Scene escena = new Scene(raiz, 420, 150);
             escena.getStylesheets().add(getClass().getResource("/com/example/ejercicios/styles/styleContador.css").toExternalForm());
-            escenarioPrincipal.setTitle("Contador");
+            escenarioPrincipal.setTitle(nomVentana);
             escenarioPrincipal.setScene(escena);
             escenarioPrincipal.show();
 
